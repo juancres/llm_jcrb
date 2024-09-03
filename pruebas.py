2e4a0d5967154fe06aafab521edbb602af5902f0
Juan c Restrepo <juakosterre@gmail.com>
2024-09-03 00:46:24 +0000

    UPDATE README.md

--- pruebas.py
+++ pruebas.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 url = 'http://localhost:11434/api/generate'
 myobj = {
@@ -6,6 +7,11 @@ myobj = {
     "prompt":  "cual es la velocidad de conexion mas rapida de internet en el mundo",
     "stream": False
 }
+
+
 x = requests.post(url, json = myobj)
-print(x.text)
+
+x = json.loads(x.text)
+
+print(x["response"])
 
