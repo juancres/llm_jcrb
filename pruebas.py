ff7dd7448191a00bdfc870c4a3af5dce506dd717
Juan c Restrepo <juakosterre@gmail.com>
2024-09-05 02:02:56 +0000

    UPDATE README.md

--- pruebas.py
+++ pruebas.py
@@ -15,3 +15,31 @@ x = json.loads(x.text)
 
 print(x["response"])
 
+
+
+
+import requests
+import json
+
+url = 'http://localhost:11434/api/generate'
+
+while True:
+    prompt = input("PROMPT: ")
+    myobj = {
+    "model": "tinyllama",
+    "prompt":  "cual es la velocidad de conexion mas rapida de internet en el mundo",
+    "stream": False
+    }
+
+headers = {
+    "Content-Type": "application/json"
+}
+
+response = requests.post(url, json = myobj, headers = headers)
+
+response = json.loads(response.text)
+
+print(x["response"])
+
+
+
